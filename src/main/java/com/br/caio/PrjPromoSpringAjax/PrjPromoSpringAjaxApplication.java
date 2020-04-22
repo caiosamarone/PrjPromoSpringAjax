4f3af5dfa13adbab81a8ef29a6c5911399cf7e57
Caio Mendes <caiosamarone10@hotmail.com>
2020-04-22 12:38:27 -0300

    Criacao do projeto

---
+++ src/main/java/com/br/caio/PrjPromoSpringAjax/PrjPromoSpringAjaxApplication.java
@@ -0,0 +1,13 @@
+package com.br.caio.PrjPromoSpringAjax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjPromoSpringAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjPromoSpringAjaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/caio/PrjPromoSpringAjax/PrjPromoSpringAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.caio.PrjPromoSpringAjax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjPromoSpringAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
